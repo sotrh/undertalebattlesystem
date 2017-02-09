f7101f01881e7b1aa307872ac514167649bdf6f7
sotrh <bhbenjaminhansen@gmail.com>
2017-02-08 21:26:05 -0700

    Player heart reduced to 10px by 10px and centered in the screen

--- core/src/sotrh/libgdx/undertalebattlesystem/Extensions.kt
+++ core/src/sotrh/libgdx/undertalebattlesystem/Extensions.kt
@@ -4,8 +4,8 @@ package sotrh.libgdx.undertalebattlesystem
  * Created by benjamin on 2/7/17
  */
 
-fun Int.toMeters() = this.toFloat() / PIXELS_TO_METERS
-fun Float.toMeters() = this / PIXELS_TO_METERS
+fun Int.toMeters() = this.toFloat() / PIXELS_PER_METERS
+fun Float.toMeters() = this / PIXELS_PER_METERS
 
-fun Int.toPixels() = this.toFloat() * PIXELS_TO_METERS
-fun Float.toPixels() = this * PIXELS_TO_METERS
+fun Int.toPixels() = this.toFloat() * PIXELS_PER_METERS
+fun Float.toPixels() = this * PIXELS_PER_METERS
--- core/src/sotrh/libgdx/undertalebattlesystem/Player.kt
+++ core/src/sotrh/libgdx/undertalebattlesystem/Player.kt
@@ -1,14 +1,25 @@
 package sotrh.libgdx.undertalebattlesystem
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.utils.Disposable
 
 /**
  * Created by benjamin on 2/7/17
  */
-class Player(val texture: Texture) : Disposable {
-    val sprite = Sprite(texture).apply { scale(0.5f) }
+class Player(val texture: Texture, var w: Float = PLAYER_SIZE, var h: Float = PLAYER_SIZE) : Disposable {
+    var x = 0f
+    var y = 0f
+
+    fun draw(spriteBatch: SpriteBatch) {
+        spriteBatch.draw(texture, x, y, w, h)
+    }
+
+    fun center() {
+        x = Gdx.graphics.width / 2 + x - w / 2
+        y = Gdx.graphics.height / 2 + y - h / 2
+    }
 
     override fun dispose() {
         texture.dispose()
